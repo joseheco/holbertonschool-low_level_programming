cfe41a674417d308f7354257d420cbdedb9269d6
joseheco <jferhc@gmail.com>
2021-02-09 18:58:07 -0500

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *_abs - function generate absolute of number.
+ *@n: input value
+ *Return: absolute value
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+n = -n;
+}
+return (n);
+}
