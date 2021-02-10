9ce0e760f44a138059be10b3074b414a9e654fe1
joseheco <jferhc@gmail.com>
2021-02-09 20:57:59 -0500

    24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * jack_bauer - prints hour min 
+ * jack_bauer - prints hour min
  *
  * Return: void
  */
@@ -14,12 +14,12 @@ for (hour = 0; hour <= 23 ; hour++)
 {
 for (min = 0; min <= 59; min++)
 {
- _putchar(hour / 10 + '0');
- _putchar(hour % 10 + '0');
- _putchar(':');
- _putchar(min / 10 + '0');
- _putchar(min % 10 + '0');
- _putchar('\n');
- }
+_putchar(hour / 10 + '0');
+_putchar(hour % 10 + '0');
+_putchar(':');
+_putchar(min / 10 + '0');
+_putchar(min % 10 + '0');
+_putchar('\n');
+}
 }
 }
