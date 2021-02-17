5a627109883c24ad182af6f0710c1c311c1171c9
joseheco <jferhc@gmail.com>
2021-02-16 21:21:55 -0500

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strlen - length string.
+ * @s: array
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+
+    int a = 0;
+
+    for(a = 0; s[a]; a++)
+    {
+        a++;
+    }
+
+    return (a);
+}
+
