3d110a071c63ba3cec06c9208701ccb6559c1ead
joseheco <jferhc@gmail.com>
2021-02-16 22:40:30 -0500

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - string
+ * @src: char
+ * @dest: char
+ * Return: return.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int a;
+int b;
+
+for (b = 0; src[b]; b++)
+{
+}
+for (a = 0; a <= b; a++)
+{
+dest[a] = src[a];
+}
+return (dest);
+}
