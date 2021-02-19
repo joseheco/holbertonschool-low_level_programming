0bba407aecf14f3e17d895a1bfb230f2bf2c63a1
joseheco <jferhc@gmail.com>
2021-02-18 23:08:49 -0500

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcat - 2 strings
+ * @dest: string
+ * @src: string
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
