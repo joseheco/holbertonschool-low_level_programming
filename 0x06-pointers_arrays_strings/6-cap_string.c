16c5dd5201903d000e018a3196c7590df4645489
joseheco <jferhc@gmail.com>
2021-02-19 16:16:34 -0500

    Capstring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -27,28 +27,27 @@ return (contador);
 
 
 char *cap_string(char *s)
-{
-int length1;
-int length2;
-int i1;
-int i2;
-char w_sp[] = {32, '\t', '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+    {
+    int length1;
+    int length2;
+    int i1;
+    int i2;
+    char w_sp[] = {32, '\t', '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-length1 = _strlen(s);
-length2 = _strlen(w_sp);
-
-for (i1 = 0; i1 < length1; i1++)
-{
-for (i2 = 0; i2 < length2; i2++)
-{
-if ((s[i1] == w_sp[i2] && (s[i1 + 1] >= 97 && s[i1 + 1] <= 122)))
-{
-s[i1 + 1] = s[i1 + 1] - 32;
-}
-else if (i1 == 0 && (s[i1] >= 97 && s[i1] <= 122))
-{
-s[i1] = s[i1] - 32;
-}
-}
-}
-}
+    length1 = _strlen(s);
+    length2 = _strlen(w_sp);
+    for (i1 = 0; i1 < length1; i1++)
+    {
+            for (i2 = 0; i2 < length2; i2++)
+            {
+                    if ((s[i1] == w_sp[i2] && (s[i1 + 1] >= 97 && s[i1 + 1] <= 122)))
+                    {
+                            s[i1 + 1] = s[i1 + 1] - 32;
+                    }
+                    else if (i1 == 0 && (s[i1] >= 97 && s[i1] <= 122))
+                    {
+                            s[i1] = s[i1] - 32;
+                    }
+            }
+    }
+    }
