778fe2c17f21b9563b0f7475d3e003bde89c727a
joseheco <jferhc@gmail.com>
2021-02-19 23:47:05 -0500

    number

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -7,27 +7,27 @@
  */
 void print_number(int n)
 {
-	int tmp;
-	int factor;
+int tmp;
+int factor;
 
-	tmp = n;
-	factor = 1;
-	while (tmp / 10)
-	{
-		tmp = tmp / 10;
-		factor *= 10;
-	}
-	while (factor > 0)
-	{
-		tmp = n / factor;
-		n = n - (tmp * factor);
-		if (tmp < 0)
-		{
-			_putchar('-');
-			tmp = -tmp;
-			n = -n;
-		}
-		_putchar(tmp + '0');
-		factor /= 10;
-	}
+tmp = n;
+factor = 1;
+while (tmp / 10)
+{
+tmp = tmp / 10;
+factor *= 10;
+}
+while (factor > 0)
+{
+tmp = n / factor;
+n = n - (tmp * factor);
+if (tmp < 0)
+{
+_putchar('-');
+tmp = -tmp;
+n = -n;
+}
+_putchar(tmp + '0');
+factor /= 10;
+}
 }
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+# define HOLBERTON_H
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -5,3 +8,9 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void _putchar(char c);
+void print_number(int n);
+
+#endif
