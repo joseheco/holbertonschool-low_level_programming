f52e2e1c345a9b9c2bc7713302b98b64deeb4164
joseheco <jferhc@gmail.com>
2021-02-22 23:48:05 -0500

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strspn - count the number 
+ * @s: string
+ * @accept: character 
+ * Return: numbers of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i = 0, j = 0;
+
+while (s[i])
+{
+for (j = 0; accept[j]; j++)
+{
+if (accept[j] == s[i])
+break;
+}
+if (accept[j] == '\0')
+return (i);
+i++;
+}
+return (0);
+}
