500beca696ba6902abc39297dc777e27dccc3b26
joseheco <jferhc@gmail.com>
2021-02-24 23:09:33 -0500

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - calculate
+ * @x: number
+ * @y: number
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+else
+return (x * _pow_recursion(x, y -1));
+}
