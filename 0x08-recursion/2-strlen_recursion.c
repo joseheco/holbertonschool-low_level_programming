1a62ccb0219147e5ebe6c5ba4a53a3269140c456
joseheco <jferhc@gmail.com>
2021-02-24 21:58:21 -0500

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - holberton
+ * @s: string
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+if (s[0])
+{
+return ( 1 + _strlen_recursion(s + 1));
+}
+return (0);
+}
