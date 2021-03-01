e4da4dc625153a331d04bebfdc2bed2a56ec2887
joseheco <jferhc@gmail.com>
2021-02-28 22:55:00 -0500

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print
+ * @argc: number
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char*argv[])
+{
+argc = 0;
+ 
+printf("%s\n",*argv);
+return 0;
+}
