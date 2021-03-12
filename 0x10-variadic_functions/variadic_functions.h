e46d676c1f5ddbd99caa0122a8c615e1453e76ca
joseheco <jferhc@gmail.com>
2021-03-11 23:20:57 -0500

    Variadic

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,23 @@
+#ifndef VARDIAC_H
+#define VARDIAC_H
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct format - Types.
+ * @form: Type symboles.
+ * @f: Function pointer.
+ *
+ **/
+typedef struct format
+{
+char form;
+void (*f)(va_list);
+} format_t;
+void print_all(const char * const format, ...);
+
+#endif
