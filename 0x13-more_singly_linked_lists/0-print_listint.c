66cc0f9bd2ea6ecbf9ca18e8b2fddb1182cb8ac9
joseheco <jferhc@gmail.com>
2021-03-23 23:37:54 -0500

    print

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h> 
+/**
+ * print_listint - Prints.
+ * @h: pointer.
+ * Return: Numbers.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+size_t counter = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+counter++;
+}
+return (counter);
+}
