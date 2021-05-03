ef39c931779ec1d8fd205dc48a5acb085c18f1d7
joseheco <jferhc@gmail.com>
2021-05-02 20:37:31 -0500

    sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node.
+ * @head: pointer.
+ * @index: index.
+ * Return: node.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int counter = 0;
+
+while ((counter < index) && head)
+{
+	head = head->next;
+	counter++;
+}
+if (counter < index)
+return (NULL);
+return (head);
+}
