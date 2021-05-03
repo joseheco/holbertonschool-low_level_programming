1e846f7af27bf9aeaa224d649f61ac9d7a3147ee
joseheco <jferhc@gmail.com>
2021-05-02 20:41:00 -0500

    sum

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,14 +9,14 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-unsigned int counter = 0;
+	unsigned int counter = 0;
 
-while ((counter < index) && head)
-{
-	head = head->next;
-	counter++;
-}
-if (counter < index)
-return (NULL);
-return (head);
+	while ((counter < index) && head)
+	{
+		head = head->next;
+		counter++;
+	}
+	if (counter < index)
+		return (NULL);
+	return (head);
 }
