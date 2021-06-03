adbbf950b577565cda36e5ee2b00f1e7a7883e56
joseheco <jferhc@gmail.com>
2021-06-03 18:12:52 -0500

    hash

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Function that prints a hash table
+ * @ht: hash table
+ * Return: 0
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int index;
+hash_node_t *node;
+
+if (ht)
+{
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+		while (node != NULL)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+			if (node)
+				printf(", ");
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
+}
