670dbecc8590e359c8398ff4df44bd3ce78e9cb1
joseheco <jferhc@gmail.com>
2021-06-03 17:49:32 -0500

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-	if (tble == NULL)
+	if (table == NULL)
 		return (NULL);
 
 	table->array = malloc(sizeof(hash_node_t *) * size);
