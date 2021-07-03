e3581d38585e5a06dc01168042638b4b4d65f60c
joseheco <jferhc@gmail.com>
2021-07-02 23:45:24 -0500

    island_perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Island_perimeter in python"""
+
+
+def island_perimeter(grid):
+    """That returns the perimeter of the island described in grid"""
+
+    count = 0
+
+    for row in grid:
+        for column in row:
+            if column == 1:
+                count += 1
+
+    if count == 0:
+        perimeter = 0
+    else:
+        perimeter = 2 * (count + 1)
+
+    return perimeter
